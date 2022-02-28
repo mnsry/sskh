697858e1ef7601d226e0b60521e75f7e05374e48
mnsry <mansory.eca@gmail.com>
2022-03-01 01:45:15 +0330

    commit

--- resources/views/page/show.blade.php
+++ resources/views/page/show.blade.php
@@ -23,33 +23,7 @@
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-12">
-                    <article class="product">
-                        <div class="row">
-                            <div class="col-lg-4 col-sm-12">
-                                <div class="product-gallery default">
-                                    <img src="{{ Voyager::Image($page->image) }}" alt="{{ $page->title }}" width="411"/>
-                                </div>
-                            </div>
-
-                            <div class="col-lg-8 col-sm-12">
-                                <div class="product-title default">
-                                    <h1><span>{{ $page->title }}</span></h1>
-                                </div>
-
-                                <div class="product-variants default">
-                                    {{ $page->excerpt }}
-                                </div>
-
-                                <div class="product-variants default">
-                                    {!! $page->body !!}
-                                </div>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-            </div>
+            
         </div>
     </main>
 
